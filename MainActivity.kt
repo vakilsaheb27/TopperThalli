10c39143ed163251c6a6564a73b89787efb54167
vakilsaheb27 <vakilsaheb27@gmail.com>
2026-04-07 04:28:14 +0530

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.messapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Text("Mess App")
+        }
+    }
+}
